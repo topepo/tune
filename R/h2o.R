5098b1824f8602b84a450b69f4b9709341966e06
qiushi <qiushi.yann@gmail.com>
2022-05-20 19:03:08 -0400

    Add extract_h2o_spec to pull algorithm name from model or workflow

--- R/h2o.R
+++ R/h2o.R
@@ -1,37 +1,89 @@
+#' Extract h2o algorithm from model and workflow
+#' @export
+extract_h2o_spec <- function(object, ...) {
+  UseMethod("extract_h2o_algorithm")
+}
+
+extract_h2o_spec.default <- function(object, ...) {
+  msg <- paste0(
+    "The first argument to [extract_h2o_algorithm] should be either ",
+    "a model or workflow."
+  )
+  rlang::abort(msg)
+}
+
+extract_h2o_spec.model_spec <- function(model_spec, ...) {
+  if (inherits(model_spec, "boost_tree")) {
+    model_name <- "boost_tree"
+    algorithm <- "gbm"
+  }
+  if (inherits(model_spec, "rand_forest")) {
+    model_name <- "rand_forest"
+    algorithm <- "randomForest"
+  }
+  if (inherits(model_spec, "linear_reg")) {
+    model_name <- "linear_reg"
+    algorithm <- "glm"
+  }
+  if (inherits(model_spec, "logistic_reg")) {
+    model_name <- "logistic_reg"
+    algorithm <- "glm"
+  }
+  if (inherits(model_spec, "multinom_reg")) {
+    model_name <- "multinom_reg"
+    algorithm <- "glm"
+  }
+  if (inherits(model_spec, "mlp")) {
+    model_name <- "mlp"
+    algorithm <- "deeplearning"
+  }
+  if (inherits(model_spec, "naive_Bayes")) {
+    model_name <- "naive_Bayes"
+    algorithm <- "naiveBayes"
+  }
+
+  list(algorithm = algorithm, model_name = model_name)
+}
+
+#' @export
+#' @rdname extract_h2o_algorithm
+extract_h2o_algorithm.workflow <- function(workflow, ...) {
+  model_spec <- hardhat::extract_spec_parsnip(workflow)
+  extract_h2o_algorithm.model_spec(model_spec)
+}
+
 as_h2o <- function(df, destination_frame_prefix) {
   as.h2o(df, destination_frame = paste(destination_frame_prefix, runif(1)))
 }
 
 is_h2o <- function(object, ...) {
-    UseMethod("is_h2o")
+  UseMethod("is_h2o")
 }
 
 is_h2o.default <- function(object, ...) {
-    msg <- paste0(
-        "The first argument to [is_h2o] should be either ",
-        "a model or workflow."
-    )
-    rlang::abort(msg)
+  msg <- paste0(
+    "The first argument to [is_h2o] should be either ",
+    "a model or workflow."
+  )
+  rlang::abort(msg)
 }
 
 is_h2o.model_spec <- function(object, ...) {
-    identical(object$engine, "h2o")
+  identical(object$engine, "h2o")
 }
 
 is_h2o.workflow <- function(object, ...) {
-    model_spec <- hardhat::extract_spec_parsnip(object)
-    identical(model_spec$engine, "h2o")
+  model_spec <- hardhat::extract_spec_parsnip(object)
+  identical(model_spec$engine, "h2o")
 }
 
 
-tune_grid_loop_iter_h2o <- function(
-    split,
-    grid_info,
-    workflow,
-    seed,
-    metrics = NULL,
-    control = control_grid()
-) {
+tune_grid_loop_iter_h2o <- function(split,
+                                    grid_info,
+                                    workflow,
+                                    seed,
+                                    metrics = NULL,
+                                    control = control_grid()) {
   load_pkgs(workflow)
   load_namespace(control$pkgs)
 
@@ -92,17 +144,20 @@ tune_grid_loop_iter_h2o <- function(
       dplyr::pull("variable")
 
     # extract hyper params into list
-    h2o_hyper_params <- purrr::map(model_param_names,
-                                   ~ dplyr::pull(iter_grid_info_models, .)
-                                   %>% unique()) %>%
+    h2o_hyper_params <- purrr::map(
+      model_param_names,
+      ~ dplyr::pull(iter_grid_info_models, .)
+      %>% unique()
+    ) %>%
       purrr::set_names(model_param_names)
 
     training_frame_processed <- as_h2o(training_frame_processed, "training_frame")
+    h2o_spec <- extract_h2o_algorithm.workflow(workflow)
     h2o_res <- h2o::h2o.grid(
-      "glm",
+      h2o_spec$algorithm,
       x = predictors,
       y = outcome,
-      training_frame = ,
+      training_frame = training_frame_processed,
       hyper_params = h2o_hyper_params
     )
 
@@ -120,8 +175,10 @@ tune_grid_loop_iter_h2o <- function(
         .iter_config
       ) %>%
       dplyr::unnest(.iter_config) %>%
-      dplyr::mutate(.pred = h2o_preds,
-             .metrics = h2o_metrics) %>%
+      dplyr::mutate(
+        .pred = h2o_preds,
+        .metrics = h2o_metrics
+      ) %>%
       dplyr::unnest(.pred)
 
     out[[iter_preprocessor]] <- grid_out
@@ -148,5 +205,3 @@ pull_h2o_metrics <- function(h2o_model, val_frame) {
   metrics <- slot(h2o::h2o.performance(h2o_model, val_frame), "metrics")
   metrics
 }
-
-
